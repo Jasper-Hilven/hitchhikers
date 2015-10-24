510f08672c660e16dda0936d2ee576ca240320d4
Jasper Hilven <jasperhilven@gmail.com>
2015-10-24 11:25:52 +0200

    Added orientation

---
+++ src/mygame/util/Orientation.java
@@ -0,0 +1,39 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package mygame.util;
+
+
+
+public class Orientation {
+    private final Rotation rotation;
+    private final Direction direction;
+  
+    public Orientation(){
+    this(Front);
+    }
+    
+  public Orientation(Direction direction){
+      this(direction, Rotation.Normal);
+  }
+  public Orientation(Direction direction, Rotation rotation){
+    this.direction = direction;
+    this.rotation = rotation;
+  }
+    
+  public enum Direction{
+    Front,
+    Left,
+    Back,
+    Right,
+    Up,
+    Down
+  }
+  public enum Rotation{
+    Normal,
+    Left,
+    UpsideDown,
+    Right
+  }
+}
